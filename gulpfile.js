53abc38576428d824a0813b91b6726d6216a8f28
Tore Myklebust <tore.myklebust@bekk.no>
2015-11-14 14:46:10 +0100

    opps

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ const gulp = require('gulp');
 const less = require('gulp-less');
 const notify = require('gulp-notify');
 
-
+var paths{
+  lessFiles = "style/**/*.less",
+  mainStyle = "style/mainstyle.less"
+}
 
 gulp.task('less', function () {
     return gulp.src(paths.mainStyle)
