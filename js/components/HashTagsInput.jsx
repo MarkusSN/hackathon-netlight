a5d1eceeb24265e160633fb2e695b8cac2845219
Tore Myklebust <tore.myklebust@bekk.no>
2015-11-14 20:02:34 +0100

    Added classnames to react compoents

--- js/components/HashTagsInput.jsx
+++ js/components/HashTagsInput.jsx
@@ -13,9 +13,10 @@ class HashTagsInput extends React.Component {
 	render() {
     const { hashTags } = this.props;
 		return (
-			<div>
-        <input type='text' value={hashTags[0]} onChange={(e) => this.onChange(0, e.target.value)}/>
-        <input type='text' value={hashTags[1]} onChange={(e) => this.onChange(1, e.target.value)}/>
+			<div className="pure-g">
+                <input className="col-lg-5-12 col-1-1" type='text' value={hashTags[0]} onChange={(e) => this.onChange(0, e.target.value)}/>
+                <span className="col-lg-1-6 col-1-1 centered-text big-vs-text">VS</span>
+                <input className="col-lg-5-12 col-1-1" type='text' value={hashTags[1]} onChange={(e) => this.onChange(1, e.target.value)}/>
 			</div>
 		);
 	}
