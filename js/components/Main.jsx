dde99795e78c6d9e09b8944bd49294130574e6ba
Markus Nilssen <markus.nilssen@bekk.no>
2015-11-14 22:27:56 +0100

    highcharsts

--- js/components/Main.jsx
+++ js/components/Main.jsx
@@ -13,10 +13,11 @@ class Main extends React.Component {
     const { hashTags, tweets, dispatch, fetchStatus } = this.props;
 		return (
 			<div>
-                <h1>What does Twitter love more??</h1>
+        <h1>What does Twitter love more??</h1>
         <HashTagsInput hashTags={hashTags} hashTagsOnChange={(hts) => dispatch(setHashTags(hts))} />
         <button className="get-tweets-button" onClick={() => dispatch(startFetching(hashTags))}>Hent</button>
-        <Speedometer />
+        <Speedometer tweets={tweets[0]} />
+        <Speedometer tweets={tweets[1]} />
         <Tweets tweets={tweets} />
 			</div>
 		);
