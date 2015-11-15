d7e304405c8f054f7a917c9b7ea7790ba4f401ae
Tore Myklebust <tore.myklebust@bekk.no>
2015-11-15 01:49:48 +0100

    Added sentiment image functionality

--- js/components/TweetList.jsx
+++ js/components/TweetList.jsx
@@ -5,11 +5,34 @@ class TweetList extends React.Component {
   }
 
   render() {
-    const { tweets } = this.props;
+      let { tweets } = this.props;
+      tweets = tweets.map(function(tweet) {
+          if (tweet.sentimentScore > 0.8) {
+              tweet.image = "images/love.png";
+          }else if (tweet.sentimentScore > 0.6) {
+              tweet.image = "images/happy.png";
+          }else if (tweet.sentimentScore > 0.4) {
+              tweet.image = "images/bad.png";
+          } else {
+              tweet.image = "images/mad.png";
+          }
+          return tweet;
+      }); 
+  
     return (
-      <ul>
-        {tweets.map((tweet) => <li key={tweet.id}>{tweet.text}</li>)}
-      </ul>
+        <ul className="col-lg-1-2 col-1-1">
+                            {tweets.map((tweet) =>
+        <li className="tweet-container" key={tweet.id}>
+            <div className="col-1-4">
+                <img className="tweet-sentiment-image" src={tweet.image} alt="" />
+            </div>
+            <div className="col-3-4">
+                <h3>{tweet.user.twitterHandle}</h3>
+                <p>{tweet.text}</p>
+            </div>
+
+        </li>)}
+                        </ul>
     );
   }
 
